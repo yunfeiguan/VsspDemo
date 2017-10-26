757238d8a4111a396eb2f8ade28256c5c343e52d
YunfeiGuan <yunfeiguan@xtaotech.com>
2017-10-26 19:35:46 +0800

    v0.7 add stat_dir.sh which can display general dir files and subdirs.

---
+++ ceph-tool/shell/stat_dir.sh
@@ -0,0 +1,21 @@
+#!/bin/bash 
+
+file=$1
+ino=`stat -c %i ${file}`
+
+function dec2hex()
+{
+	printf "%0x" $1
+}
+
+ino_hex=$(dec2hex $ino)
+
+oid="${ino_hex}.00000000"
+
+echo $oid
+
+#get omapheader 
+rados -p cephfs_metadata getomapheader $oid /tmp/kkkkk --cluster xtao
+ceph-dencoder import /tmp/kk type fnode_t  decode dump_json 
+
+rm -rf /tmp/kkkkk
