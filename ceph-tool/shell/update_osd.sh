c5c30499deae1ce5b9550dfb545242f8313bf244
IvanGuan <yunfei.guan@xtaotech.com>
2023-01-28 16:28:28 +0800

    V0.47 checked in by Alwyn
    optimize update_osd.sh

--- ceph-tool/shell/update_osd.sh
+++ ceph-tool/shell/update_osd.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 #mount_uuid=$1
-list=`docker ps -a |grep xtao-osd | awk -F ' ' '{print $10}' | awk -F '.' '{print $2}'`
+IMAGE_NAME="ceph-n-jwd_mempool"
+#list=`docker ps -a |grep xtao-osd | awk -F ' ' '{print $10}' | awk -F '.' '{print $2}'`
+list=`ls /var/lib/ceph/osd/ | awk -F '-' '{print $2}'`
 
 for i in ${list[@]} 
 do
@@ -16,7 +18,7 @@ do
 # create new osd container
   echo "docker create --net=host --pid=host -v /mnt/${mount_uuid}:/mnt/${mount_uuid}:shared -v /var/lib/ceph/osd:/var/lib/ceph/osd:shared -v /var/crash:/var/crash:shared -v /var/run/ceph:/var/run/ceph:shared -v /var/log/ceph:/var/log/ceph:shared -v /etc/ceph:/etc/ceph -v /dev:/dev:shared -v /entrypoint --privileged -e CLUSTER=xtao -e OSD_ID=${osd_num}  --name=xtao-osd.${osd_num} ceph osd"
 
-  docker create --net=host --pid=host -v /mnt/${mount_uuid}:/mnt/${mount_uuid}:shared -v /var/lib/ceph/osd:/var/lib/ceph/osd:shared -v /var/crash:/var/crash:shared -v /var/run/ceph:/var/run/ceph:shared -v /var/log/ceph:/var/log/ceph:shared -v /etc/ceph:/etc/ceph -v /dev:/dev:shared -v /entrypoint --privileged -e CLUSTER=xtao -e OSD_ID=${osd_num}  --name=xtao-osd.${osd_num} ceph osd
+  docker create --net=host --pid=host -v /mnt/${mount_uuid}:/mnt/${mount_uuid}:shared -v /var/lib/ceph/osd:/var/lib/ceph/osd:shared -v /var/crash:/var/crash:shared -v /var/run/ceph:/var/run/ceph:shared -v /var/log/ceph:/var/log/ceph:shared -v /etc/ceph:/etc/ceph -v /dev:/dev:shared -v /entrypoint --privileged -e CLUSTER=xtao -e OSD_ID=${osd_num}  --name=xtao-osd.${osd_num} ${IMAGE_NAME} osd
 
 # start new osd container
   systemctl reset-failed xtao-osd.${osd_num}.service
